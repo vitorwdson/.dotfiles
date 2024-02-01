f133b9a5e2a84936151ec40945903e775b5b439a
Vitor Wdson <vitor.wdson2@gmail.com>
2024-01-31 20:58:34 -0400

    Install script for .config and .local

--- install.sh
+++ install.sh
@@ -1,3 +1,38 @@
 #!/usr/bin/env bash
 
-git clone git@github.com:vitorwdson/nvim-config.git ./config/nvim
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if [[ ! -d "$SCRIPT_DIR/config/nvim" ]]; then
+    git clone git@github.com:vitorwdson/nvim-config.git "$SCRIPT_DIR/config/nvim"
+fi
+
+if [[ ! -d "$SCRIPT_DIR/backup" ]]; then
+    mkdir "$SCRIPT_DIR/backup"
+    mkdir "$SCRIPT_DIR/backup/.config"
+    mkdir "$SCRIPT_DIR/backup/.local"
+fi
+
+function install() {
+    local path="$1"
+    local name=$(basename "$path")
+    local destiny_folder="$2"
+    local destiny="$HOME/$destiny_folder/$name"
+    local backup="$SCRIPT_DIR/backup/$destiny_folder/$name"
+
+    if [[ -e "$destiny" ]]; then
+        cp -r "$destiny" "$backup"
+        rm -rf "$destiny"
+    fi
+
+    ln -s "$path" "$destiny"
+}
+
+configs=($SCRIPT_DIR/config/*)
+for f in "${configs[@]}"; do
+    install "$f" ".config"
+done
+
+locals=($SCRIPT_DIR/local/*)
+for f in "${locals[@]}"; do
+    install "$f" ".local"
+done
