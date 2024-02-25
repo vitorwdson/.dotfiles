a3b3bdf690a8474c8919f1ca116a2f65eba9de33
Vitor Wdson <vitor.wdson2@gmail.com>
2024-02-25 14:39:22 -0400

    feat: add sddm tokyo night theme

--- arch-linux/hyprland.sh
+++ arch-linux/hyprland.sh
@@ -3,9 +3,15 @@ sudo pacman -S qt5-graphicaleffects qt5-quickcontrols2 qt5-wayland qt6-wayland \
 
 yay -S hyprland-git hyprpaper-git hyprpicker hyprshot-git mako-git \
     swaylock-effects-git thunar-csd waybar-git wl-clipboard-git wlogout \
-    xdg-desktop-portal-hyprland-git
+    xdg-desktop-portal-hyprland-git sddm-theme-tokyo-night
 
 gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
+
+sudo mkdir -p /etc/sddm.conf.d
+sudo cp /usr/lib/sddm/sddm.conf.d/default.conf /etc/sddm.conf.d/
+sudo sed -i 's@^Current=$@Current=tokyo-night-sddm@' /etc/sddm.conf.d/default.conf
+sudo sed -i 's@^Numlock=none$@Numlock=on@' /etc/sddm.conf.d/default.conf
+sudo sed -i 's@Background="Backgrounds/win11.png"@Background="Backgrounds/shacks.png"@' /usr/share/sddm/themes/tokyo-night-sddm/theme.conf
 
 echo ""
 read -p "Do you wish to enable sddm? (y/N): " confirm
