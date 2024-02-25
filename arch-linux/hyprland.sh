082e26e026f76366f17e0776f58b100f9ce07c00
Vitor Wdson <vitor.wdson2@gmail.com>
2024-02-25 18:10:59 -0400

    feat: install adwaita gtk and fix sddm.sh call

--- arch-linux/hyprland.sh
+++ arch-linux/hyprland.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo pacman -S qt5-graphicaleffects qt5-quickcontrols2 qt5-wayland qt6-wayland \
-    wofi xdg-desktop-portal-gtk xdg-utils dolphin glib2
+    wofi xdg-desktop-portal-gtk xdg-utils dolphin glib2 gnome-themes-extra
 
 yay -S hyprland-git hyprpaper-git hyprpicker hyprshot-git mako-git \
     swaylock-effects-git thunar-csd waybar-git wl-clipboard-git wlogout \
@@ -9,4 +9,6 @@ yay -S hyprland-git hyprpaper-git hyprpicker hyprshot-git mako-git \
 
 gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
 
-./sddm.sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+$SCRIPT_DIR/sddm.sh
