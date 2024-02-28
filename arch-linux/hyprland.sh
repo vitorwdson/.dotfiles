594e802eeb2f8a90a9c3d4b58a9790d2327c2c79
Vitor Wdson <vitor.wdson2@gmail.com>
2024-02-27 21:52:52 -0400

    feat: remove wlogout

--- arch-linux/hyprland.sh
+++ arch-linux/hyprland.sh
@@ -4,7 +4,7 @@ sudo pacman -S qt5-graphicaleffects qt5-quickcontrols2 qt5-wayland qt6-wayland \
     wofi xdg-desktop-portal-gtk xdg-utils dolphin glib2 gnome-themes-extra
 
 yay -S hyprland-git hyprpaper-git hyprpicker hyprshot-git mako-git \
-    swaylock-effects-git thunar-csd waybar-git wl-clipboard-git wlogout \
+    swaylock-effects-git thunar-csd waybar-git wl-clipboard-git \
     xdg-desktop-portal-hyprland-git rofi-lbonn-wayland
 
 gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
