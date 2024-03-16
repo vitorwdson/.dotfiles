b31ab82011c757f26aac6b36c19ded9a7d41bc49
Vitor Wdson <vitor.wdson2@gmail.com>
2024-03-15 20:27:57 -0400

    feat: removed oh-my-tmux and added tpm

--- arch-linux/core.sh
+++ arch-linux/core.sh
@@ -7,4 +7,4 @@ sudo pacman -S base base-devel zsh firefox fzf gamemode git grub htop \
     ttf-roboto unzip vlc wget wireplumber neofetch flatpak w3m bat socat lsd
 
 yay -S heroic-games-launcher-bin otf-font-awesome-5 snapd solaar-git spotify \
-    vesktop-bin xwaylandvideobridge-git nb
+    vesktop-bin xwaylandvideobridge-git nb tmux-plugin-manager
