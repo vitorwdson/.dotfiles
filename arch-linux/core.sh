109553f7dfcc378323a739e2c7d80eb8039c8297
Vitor Wdson <vitor.wdson2@gmail.com>
2024-03-20 18:08:34 -0400

    feat: install latest neovim from aur

--- arch-linux/core.sh
+++ arch-linux/core.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 sudo pacman -S base base-devel zsh firefox fzf gamemode git grub htop \
-    kitty man-db mangohud neovim nfs-utils noto-fonts noto-fonts-cjk \
+    kitty man-db mangohud nfs-utils noto-fonts noto-fonts-cjk \
     noto-fonts-emoji os-prober pipewire pipewire-alsa pipewire-jack \
     pipewire-pulse  ripgrep steam tmux ttf-dejavu ttf-liberation \
     ttf-roboto unzip vlc wget wireplumber neofetch flatpak w3m bat socat lsd
 
 yay -S heroic-games-launcher-bin otf-font-awesome-5 snapd solaar-git spotify \
-    vesktop-bin xwaylandvideobridge-git nb tmux-plugin-manager
+    vesktop-bin xwaylandvideobridge-git nb tmux-plugin-manager neovim-git
