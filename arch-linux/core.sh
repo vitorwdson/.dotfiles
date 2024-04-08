b0f5433f743ff54db884127a0b6a51ec1a3235dd
Vitor Wdson <vitor.wdson2@gmail.com>
2024-04-08 19:18:40 -0400

    feat: updated xwaylandvideobridge

--- arch-linux/core.sh
+++ arch-linux/core.sh
@@ -4,7 +4,8 @@ sudo pacman -S base base-devel zsh firefox fzf gamemode git grub htop \
     kitty man-db mangohud nfs-utils noto-fonts noto-fonts-cjk \
     noto-fonts-emoji os-prober pipewire pipewire-alsa pipewire-jack \
     pipewire-pulse  ripgrep steam tmux ttf-dejavu ttf-liberation \
-    ttf-roboto unzip vlc wget wireplumber neofetch flatpak w3m bat socat lsd
+    ttf-roboto unzip vlc wget wireplumber neofetch flatpak w3m bat socat lsd \
+    xwaylandvideobridge
 
 yay -S heroic-games-launcher-bin otf-font-awesome-5 snapd solaar-git spotify \
-    vesktop-bin xwaylandvideobridge-git nb tmux-plugin-manager neovim-git
+    vesktop-bin nb tmux-plugin-manager neovim-git
