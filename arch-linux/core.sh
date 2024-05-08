38aadf804bf5b3a7dcf9143c64fe46a71e9918a0
Vitor Wdson <vitor.wdson2@gmail.com>
2024-05-07 21:52:30 -0400

    feat: install wine and lutris

--- arch-linux/core.sh
+++ arch-linux/core.sh
@@ -5,7 +5,7 @@ sudo pacman -S base base-devel zsh firefox fzf gamemode git grub htop \
     noto-fonts-emoji os-prober pipewire pipewire-alsa pipewire-jack \
     pipewire-pulse  ripgrep steam tmux ttf-dejavu ttf-liberation \
     ttf-roboto unzip vlc wget wireplumber neofetch flatpak w3m bat socat lsd \
-    xwaylandvideobridge
+    xwaylandvideobridge lutris wine winetricks
 
 yay -S heroic-games-launcher-bin otf-font-awesome-5 snapd solaar-git spotify \
     vesktop-bin nb tmux-plugin-manager neovim-git
