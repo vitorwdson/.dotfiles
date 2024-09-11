5f95dd68c366077d79bc54fa653194dae95a72b4
Vitor Wdson <vitor.wdson2@gmail.com>
2024-09-10 22:16:43 -0400

    feat: install fzf, rg and fd

--- arch-linux/core.sh
+++ arch-linux/core.sh
@@ -6,7 +6,7 @@ sudo pacman -S base base-devel zsh firefox fzf gamemode git grub htop \
     pipewire-pulse  ripgrep steam tmux ttf-dejavu ttf-liberation \
     ttf-roboto unzip vlc wget wireplumber neofetch flatpak w3m bat socat lsd \
     xwaylandvideobridge lutris wine winetricks cups cups-pdf df nss-mdns avahi \
-    neovim
+    neovim fzf rg fd
 
 yay -S heroic-games-launcher-bin otf-font-awesome-5 snapd solaar-git spotify \
     vesktop-bin nb tmux-plugin-manager
