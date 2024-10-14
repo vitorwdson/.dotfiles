023fcf73b9bbadd2dde5d3c4144a6f93ecc8649b
Vitor Wdson <vitor.wdson2@gmail.com>
2024-10-13 22:58:46 -0400

    feat: keychain and postgres pg_hba

--- nixos/programs.nix
+++ nixos/programs.nix
@@ -27,6 +27,7 @@
 		vesktop
 		spotify
 		pamixer
+		keychain
 
 		gamemode
 		mangohud
@@ -143,8 +144,17 @@
 		enable = true;
 		ensureDatabases = [];
 		authentication = pkgs.lib.mkOverride 10 ''
-			#type database  DBuser  auth-method
-			local all       all     trust
+			# TYPE  DATABASE        USER            ADDRESS                 METHOD
+			# "local" is for Unix domain socket connections only
+			local   all             all                                     trust
+			# IPv4 local connections:
+			host    all             all             127.0.0.1/32            trust
+			host    all             all             127.0.0.1/32            md5
+			host    all             all             127.0.0.1/32            scram-sha-256
+			# IPv6 local connections:
+			host    all             all             ::1/128                 trust
+			host    all             all             ::1/128                 md5
+			host    all             all             ::1/128                 scram-sha-256
 		'';
 	};
 }
